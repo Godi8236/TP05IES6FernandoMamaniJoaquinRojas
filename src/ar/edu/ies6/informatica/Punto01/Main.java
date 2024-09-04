dc4e262e1bafd3f38887ddecc0b25812451e2a79
Godi8236 <fernandomamani8236@gmail.com>
2024-09-04 17:57:30 -0300

    Ejercicio01 realizado - Fernando Mamani y Joaquin Rojas

---
+++ src/ar/edu/ies6/informatica/Punto01/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.ies6.informatica.Punto01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Definicion de las varables
+		String nombrePais = "Argentina";
+		int edad = 25;
+		double alturaEdificio = 180.5;
+		double precio = 1050.50;
+		//long numeroTelefono = 3884609238;
+		double coseno = Math.cos(0.5);
+		
+		int numero1 = 10;
+		int numero2 = 6;
+		int numero3 = 8;
+		int numero4 = 7;
+		int numero5 = 10;
+		
+		int sumaTotal = numero1+numero2+numero3+numero4+numero5;
+		int promedio = sumaTotal / 5;
+		System.out.println("El promedio es: "+promedio);
+	}
+
+}
