c553b76fbda88c106967ad512bc1b18ca6230e39
Godi8236 <fernandomamani8236@gmail.com>
2024-09-05 16:06:21 -0300

    Agregado de los comentarios -- Fernando Mamani

--- src/ar/edu/ies6/informatica/Punto01/Main.java
+++ src/ar/edu/ies6/informatica/Punto01/Main.java
@@ -4,22 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		//Definicion de las varables
+		
 		String nombrePais = "Argentina";
 		int edad = 25;
 		double alturaEdificio = 180.5;
 		double precio = 1050.50;
 		//long numeroTelefono = 3884609238;
 		double coseno = Math.cos(0.5);
-		
+		//Numeros para calcular el promedio
 		int numero1 = 10;
 		int numero2 = 6;
 		int numero3 = 8;
 		int numero4 = 7;
 		int numero5 = 10;
-		
+		//Realiza la suma de todas las notas
 		int sumaTotal = numero1+numero2+numero3+numero4+numero5;
+		//Relaiza el calculo del promedio 
 		int promedio = sumaTotal / 5;
+		//Muestra el resultado por pantalla
 		System.out.println("El promedio es: "+promedio);
 	}
 
--- src/ar/edu/ies6/informatica/Punto02/Main.java
+++ src/ar/edu/ies6/informatica/Punto02/Main.java
@@ -1,9 +1,12 @@
 package ar.edu.ies6.informatica.Punto02;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
